2b82743e5b8e67a4a2bbaf1b6d081b11ebc6da75
Igor <rozhkovigor@yandex.ru>
2020-05-23 18:08:09 +0300

    added logging

--- users-service-api/build.gradle.kts
+++ users-service-api/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.github.microutils:kotlin-logging:1.7.9")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
--- users-service-api/src/main/kotlin/ru/mipt/tpos/kubernetes/users/UsersService.kt
+++ users-service-api/src/main/kotlin/ru/mipt/tpos/kubernetes/users/UsersService.kt
@@ -1,17 +1,27 @@
 package ru.mipt.tpos.kubernetes.users
 
+import mu.KotlinLogging
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+
+private val log = KotlinLogging.logger { }
 
 @Service
 class UsersService(
     private val usersRepository: UsersRepository
 ) {
-    fun getUserById(id: Long): User? =
-        usersRepository.findByIdOrNull(id)
+    fun getUserById(id: Long): User? {
+        log.info { "Trying to get user by id $id" }
+        val user = usersRepository.findByIdOrNull(id)
+        log.info { "User with id $id is $user" }
+        return user
+    }
 
     fun addUser(name: String, age: Int): Long {
+        log.info { "Adding user with name $name and age $age" }
         val user = User(name, age)
-        return usersRepository.save(user).id
+        val savedUser = usersRepository.save(user)
+        log.info { "User with name $name and age $age successfully added: $savedUser" }
+        return savedUser.id
     }
 }
