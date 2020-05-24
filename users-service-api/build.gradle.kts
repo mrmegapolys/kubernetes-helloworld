cbf1cadeeb59f30e6f257b34b106ed239f4b683d
Igor <rozhkovigor@yandex.ru>
2020-05-24 08:43:59 +0300

    added healthcheck

--- users-service-api/build.gradle.kts
+++ users-service-api/build.gradle.kts
@@ -5,6 +5,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     runtimeOnly("org.postgresql:postgresql")
 }
